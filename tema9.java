82afad0db0bb4848003848019557e34e76d04c5e
Nico2795 <105306980+Nico2795@users.noreply.github.com>
2022-06-22 18:11:37 -0400

    Update tema9.java

--- tema9.java
+++ tema9.java
@@ -1,15 +1,22 @@
+pimport java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Cliente cliente = new Cliente ();
-        cliente.nombre = "Me llamo Nico";
+        Trabajador trabajador = new Trabajador();
+        cliente.nombre = "Nico";
         cliente.edad = 15;
         cliente.telefono = 3312;
         cliente.credito = "santander";
         
-        System.out.println(cliente.edad);
-        System.out.println(cliente.nombre);
-        System.out.println(cliente.telefono);
-        System.out.println(cliente.credito);
+      System.out.println("Soy " + cliente.nombre + ", tengo " + cliente.edad + " años , " + "mi numero de telefono es  " + cliente.telefono + ", y mi credito es del " + cliente.credito);
+      
+        trabajador.nombre = "Barbi";
+        trabajador.edad = 26;
+        trabajador.telefono= 7618;
+        trabajador.salario = 5000000;
+        
+        System.out.println("Soy " + trabajador.nombre + ", tengo" + trabajador.edad + " años, " + "mi numero de telefono es " + trabajador.telefono + ", y mi sueldo es " + trabajador.salario);
         
         
 
