f4c916dbdebde7f463d6680b736aece315665cb2
Nico2795 <105306980+Nico2795@users.noreply.github.com>
2022-06-30 02:49:14 -0400

    Create Ejercicio1.py

---
+++ Ejercicios python/Tema 5/Ejercicio1.py
@@ -0,0 +1,12 @@
+
+def areatriangulo (altura , base):
+    calculo = (altura * base) / 2
+    print(calculo)
+
+areatriangulo(,) 
+
+def areaCirculo (radio):
+    area = pi * radio**2
+    print(area)
+
+areaCirculo()
