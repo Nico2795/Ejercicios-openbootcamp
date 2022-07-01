9d264d308cdde2c1a6aee85a9e612131fe30d277
Nico2795 <105306980+Nico2795@users.noreply.github.com>
2022-07-01 16:50:10 -0400

    Update Ejercicio2.py

--- Ejercicios python/Tema 6/Ejercicio2.py
+++ Ejercicios python/Tema 6/Ejercicio2.py
@@ -3,10 +3,7 @@ class Alumno :
     def __init__(self, nombre, nota):
         self.nombre = nombre
         self.nota = nota
-    def imprimir(self):
-         print("Nombre:", self.nombre)
-         print("Nota:", self.nota)
-    def notas (self):
+   
         if self.nota >= 50 :
             print("Felicidades",self.nombre , "Has aprobado")
 
@@ -17,9 +14,3 @@ class Alumno :
 
 alumno1 =Alumno("Nico", 100)
 alumno2=Alumno("Barbi", 40)
-
-alumno1.imprimir()
-alumno1.notas()
-alumno2.imprimir()
-alumno2.notas()
-
