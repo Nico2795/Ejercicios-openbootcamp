3972b37b29ed24cdefdad2e29f0f21581e9ae24f
Nico2795 <105306980+Nico2795@users.noreply.github.com>
2022-07-04 20:48:31 -0400

    Create Ejercicio 1.py

---
+++ Ejercicios python/Tema 7/Ejercicio 1.py
@@ -0,0 +1,13 @@
+import operaciones as ope
+
+
+print("La suma es" , ope.suma(4,3))
+print("La resta es " , ope.resta(5,3))
+print("La division es" ,ope.dividir(4,2))
+print("La multiplicacion es " , ope.multiplicar(6,7))
+
+
+La suma es 7
+La resta es  2
+La division es 2.0
+La multiplicacion es  42
